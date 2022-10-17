2d129697d24ee02f1d5e9830ad154e51773d8235
aser <aser.cortines@multiversecomputing.com>
2022-10-17 10:18:05 +0200

    create function to draw point in canvas new imports changes in end point to take into account new function

--- ASCII-plotter/stOutCanvas.py
+++ ASCII-plotter/stOutCanvas.py
@@ -3,6 +3,8 @@ Functions to initialize and manipulate StOut canvas
 """
 
 import numpy as np
+from numpy.typing import NDArray
+from typing import Tuple
 from color import color
 from stOutPlotter import print_chart
 
@@ -10,7 +12,7 @@ from stOutPlotter import print_chart
 def make_canvas(width: int,
                 height: int,
                 display_grid: bool = False,
-                secondary_color: tuple = (125, 125, 125)) -> np.typing.NDArray:
+                secondary_color: tuple = (125, 125, 125)) -> NDArray:
     """
     Create a function for initializing a 2D array based on given dimensions width and height.
     :param width:
@@ -32,9 +34,42 @@ def make_canvas(width: int,
     return canvas
 
 
+def draw_in_canvas(canvas: NDArray,
+                   *values: Tuple[int],
+                   primary_color: Tuple[int] = (0, 0, 255),
+                   display_grid: bool = False) -> NDArray:
+
+    """
+    Draw values in canvas
+    :param canvas:
+    :param values:
+    :param primary_color:
+    :param display_grid:
+    :return: Canvas (NDArray) with plotted data
+    """
+
+    height, width = canvas.shape
+    dot = color("*", primary_color) if display_grid else color(".", primary_color)
+
+    for val in values:
+        x, y = val
+        canvas[height - y - 1, x] = dot
+
+    return canvas
+
+
 if __name__ == "__main__":
+
+
 
     print_chart(make_canvas(width=10, height=4))
     print()
-    print_chart(make_canvas(10, 5, display_grid=True))
+    test_canvas = make_canvas(10, 5, display_grid=True)
+    print_chart(test_canvas)
+
+    print()
+    print_chart(draw_in_canvas(test_canvas, (5, 2), (6, 3), display_grid=True))
+
+
+
 
